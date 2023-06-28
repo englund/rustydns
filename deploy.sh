3675a048cb248fde740145a0a6e337a21f1d4043
Victor Englund <victor.englund@gmail.com>
2023-06-28 19:44:04 +0200

    rename deploy package

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-scp target/aarch64-unknown-linux-musl/release/ydns root@192.168.1.1:/usr/local/bin/ydns
+scp target/aarch64-unknown-linux-musl/release/ydns root@192.168.1.1:/usr/local/bin/ydns-updater
