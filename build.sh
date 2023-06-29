d857161e5b9bdea0cfc25be7b078a3f4c3c37978
Victor Englund <victor.englund@gmail.com>
2023-06-29 19:12:21 +0200

    remove build and deploy from git

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-cargo build --target=aarch64-unknown-linux-musl --release
--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-scp target/aarch64-unknown-linux-musl/release/ydns-updater root@192.168.1.1:/usr/local/bin/ydns-updater
