1d65f9e0a7d7389ed380dacd73e7212bee4ffd9a
Victor Englund <victor.englund@gmail.com>
2024-04-01 19:09:00 +0200

    Add golden path unit test for ydns client

--- src/ydns_client.rs
+++ src/ydns_client.rs
@@ -27,7 +27,7 @@ pub async fn update_host(
     {
         Ok(response) => match response.text().await {
             Ok(r) => {
-                if !r.contains("ok") || !r.contains("nochg") {
+                if !r.contains("ok") && !r.contains("nochg") {
                     return Err(format!("Unexpected response: {}", r).into());
                 }
                 return Ok(r);
@@ -39,5 +39,50 @@ pub async fn update_host(
         Err(e) => {
             return Err(format!("Something went terrible wrong! Error: {}", e).into());
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use base64::prelude::*;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get_current_ip() {
+        let mut server = mockito::Server::new_async().await;
+        let url = server.url();
+        let _mock = server
+            .mock("GET", "/ip")
+            .with_status(200)
+            .with_body("1.3.3.7")
+            .create();
+
+        let ip = get_current_ip(&url).await.unwrap();
+        assert_eq!(ip, "1.3.3.7");
+    }
+
+    #[tokio::test]
+    async fn test_update_host() {
+        let username = "test";
+        let password = "test";
+        let auth_header = BASE64_STANDARD.encode(format!("{}:{}", username, password));
+        let host = "example.com";
+        let ip = "1.3.3.7";
+
+        let mut server = mockito::Server::new_async().await;
+        let url = server.url();
+        let _mock = server
+            .mock("GET", format!("/update/?host={}&ip={}", host, ip).as_str())
+            .match_header("Authorization", format!("Basic {}", auth_header).as_str())
+            .with_status(200)
+            .with_body("ok")
+            .create();
+
+        let response = update_host(&url, &username, &password, &host, &ip)
+            .await
+            .unwrap();
+
+        assert_eq!(response, "ok");
     }
 }
